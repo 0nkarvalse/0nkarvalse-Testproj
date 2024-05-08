4559e6d872cb2441cf962ec9edc01d532b1941b8
0nkarvalse <0nkarvalse>
2024-05-08 22:52:33 +0530

    1st templates added

--- proj/test2/views.py
+++ proj/test2/views.py
@@ -1,7 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
+from django.template import loader
+from django.http import Http404
+
 
 # Create your views here.
 
 def sayHello(request):
     return HttpResponse('<h1 style="color:red">Welcome in Aurora.</h1>')
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+# def results(request, question_id):
+#     response = "You're looking at the results of question %s."
+#     return HttpResponse(response % question_id)
+
+
+# def vote(request, question_id):
+#     return HttpResponse("You're voting on question %s." % question_id)
+
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "test2/index.html", context)
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "test2/detail.html", {"question": question})
