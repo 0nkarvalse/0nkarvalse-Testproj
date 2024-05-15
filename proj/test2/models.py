3aefe07617a557b6ad9e98a39313aa6814ff08e7
0nkarvalse <0nkarvalse>
2024-05-15 12:46:32 +0530

    field added

--- proj/test2/models.py
+++ proj/test2/models.py
@@ -3,8 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
+    question_text = models.CharField(max_length=200, blank=False)
     pub_date = models.DateTimeField("date published")
+    is_active = models.BooleanField(default=True)
+    views = models.IntegerField(default=0)
+    category = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return self.question_text
@@ -12,6 +15,11 @@ class Question(models.Model):
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+    votes = models.IntegerField(default=0,)
+    is_correct = models.BooleanField(default=False)
+    feedback = models.TextField(blank = True, null = True)
 
-    #Incorrect#20
+    def __str__(self):
+        return f"{self.choice_text} (Question: {self.question.question_text})"
+
+    # Incorrect#20
